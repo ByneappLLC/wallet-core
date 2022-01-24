b4e026fe894bec90e16ddddeca0f7f1f44efbf13
Edgar <edgrr.trem000@gmail.com>
2022-01-24 11:22:33 +0700

    rudimentary codegen

--- dart/lib/dart.dart
+++ dart/lib/dart.dart
@@ -1,12 +1,14 @@
-
 import 'dart:async';
 
+import 'package:dart/generated_bindings.dart';
 import 'package:flutter/services.dart';
 
 class Dart {
   static const MethodChannel _channel = MethodChannel('dart');
 
   static Future<String?> get platformVersion async {
+    final t = TrustWallet(dynamicLibrary);
+
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
