fac9b393423eff8052ce7ffe38c297c31cb63978
Edgar <edgrr.trem000@gmail.com>
2022-04-01 16:38:25 +0700

    regen for android

--- dart/android/src/main/kotlin/com/byneapp/wallet_core/WalletCorePlugin.kt
+++ dart/android/src/main/kotlin/com/byneapp/wallet_core/WalletCorePlugin.kt
@@ -10,6 +10,9 @@ import io.flutter.plugin.common.MethodChannel.Result
 
 /** WalletCorePlugin */
 class WalletCorePlugin: FlutterPlugin, MethodCallHandler {
+    init {
+        System.loadLibrary("TrustWalletCore")
+    }
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
